c52c843007852d8d28e70b07f3eaa9d96e4477e8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-23 03:09:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function theBeatlesPlay(){
       return `${musicians[i]} plays ${instruments[i]}`;
   }
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+var i = 0;
+while(i < facts.length){
+  return i + "!!!";
+}
