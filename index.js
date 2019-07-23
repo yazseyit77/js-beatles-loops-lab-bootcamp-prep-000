3b67ff9379d31284a22e0d3a09148553b4a036c2
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-23 02:55:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(){
   for(var i = 0; i < musicians.length; i++){
-      return `'${musicians[i]} plays ${instruments[i]}'`;
+      return `${musicians[i]} plays ${instruments[i]}`;
   }
 }
