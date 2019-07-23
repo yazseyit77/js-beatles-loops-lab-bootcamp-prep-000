49bc4d96c253d33bbde783f8ad9980d6dea5ebd7
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-23 02:45:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // add solution here
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-var array3 = [];
 
 function theBeatlesPlay(musicians, instruments){
   for(var i = 0; i < musicians.length; i++){
-      console.log(array1[0] + "plays " array2[0]);
+      console.log(musicians[0] + "plays " instruments[0]);
   }
 }
