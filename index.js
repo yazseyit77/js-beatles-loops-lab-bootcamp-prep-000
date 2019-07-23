e724302b6613ea617797e19100ce61b932b30dbc
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-23 02:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+array1 = {"John Lennon", "Beatles", "The Eagles", "Bon Jovi"};
+array2 = {"guitar", "piano", "acoustic guitar", "electric guitar"};
+
+function theBeatlesPlay(array1, array2){
+  var array1 = {};
+  
+}
