82b65c6b9b1c58d1dd0e0d67e3faf59d3767d380
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-23 02:45:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // add solution here
-var array1 = ["John Lennon", "Beatles", "The Eagles", "Bon Jovi"];
-var array2 = ["guitar", "piano", "acoustic guitar", "electric guitar"];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 var array3 = [];
 
-function theBeatlesPlay(array1, array2){
-  for(var i = 0; i < array1.length; i++){
+function theBeatlesPlay(musicians, instruments){
+  for(var i = 0; i < musicians.length; i++){
       console.log(array1[0] + "plays " array2[0]);
   }
 }
